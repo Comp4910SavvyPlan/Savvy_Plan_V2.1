c11b15378693da5ebbd8899c03e3690a11ef1a0c
Caleb <calebbelcourt@hotmail.com>
2020-02-18 12:25:27 -0800

    Charts
    Added 2 charts and styled them on page

--- src/pages/spending/Spending.js
+++ src/pages/spending/Spending.js
@@ -2,6 +2,8 @@ import React, {useState} from "react"
 import styled from "styled-components"
 import Header from "pages/spending/components/Header"
 import ControlPanel from "pages/spending/components/ControlPanel"
+import chart from "assets/images/spending_chart.png"
+import circle_chart from "assets/images/spending_chart_circle.png"
 
 
 const Spending = () => {
@@ -12,6 +14,12 @@ const Spending = () => {
         <Page>
             <Header monthly={monthly} toggleMonthly={toggleMonthly}/>
             <ChartWrapper>
+              <CircleChartPlaceHolder>
+                <img src={circle_chart}/>
+              </CircleChartPlaceHolder>
+              <ChartPlaceHolder>
+                <img src={chart}/>
+              </ChartPlaceHolder>
             </ChartWrapper>
             <ControlPanel/>
 
@@ -27,15 +35,29 @@ export default Spending
 
 const Page = styled.div`
     ${props => props.theme.pageBaseStyles}
-    grid-template-rows: minmax(12rem, 14rem)  minmax(24rem, 28rem);
-    grid-template-areas: 
-    "a a a a "
-    "b b b b "
-    "c c c c "
+    grid-template-rows: minmax(7rem, 10rem) minmax(21rem, 22rem) minmax(28rem, 40rem);
+    width: 100%;
+    position: relative;
+    grid-template-areas:
+    'a a a a a a a a a a a a'
+    'b b b b b b b b b b b b'
+    'c c c c c c c c c c c c'
 `
 
 const ChartWrapper = styled.div`
     grid-area: b;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: row;
+`
 
-
+const CircleChartPlaceHolder = styled.div`
+    width: 30%;
+    height: 90%;
+`
+const ChartPlaceHolder = styled.div`
+    width: 70%;
+    height: 100%;
 `
