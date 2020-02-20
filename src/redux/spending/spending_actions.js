89650b14840cf20b40888ecaeb6bd9703dcc62f1
Sho <shoyu99@hotmail.com>
2020-02-19 23:09:10 -0800

    Structure of action and reducer file made

--- src/redux/spending/spending_actions.js
+++ src/redux/spending/spending_actions.js
@@ -1,11 +1,21 @@
 import _ from "lodash"
 
-export const setSpendingValue_action = (logValue, rangeBarValue, rangeBarProps) => ({
+export const setSpendingValue_action = (logValue, rangeBarValue, category, rangeBarProps, id) => {
+
+        return ({
         type: "spending_reducer/SET_VALUE",
-        id: rangeBarProps.id,
-        category: rangeBarProps.category,
+        id: spendingId,
+        category: category,
         financialValue: logValue,
         rangeBarValue,
+        rangeBarProps
+})}
+
+export const changeLabel_action = (e, rangeBarProps, spendingId) => ({
+        type: "spending_reducer/CHANGE_LABEL",
+        id: spending,
+        label: e.target.value,
+        category: rangeBarProps.category,
         rangeBarProps
 })
 
@@ -14,3 +24,12 @@ export const removeSpending_action = (rangeBarProps) => ({
         id: rangeBarProps.id,
         category: rangeBarProps.category,
 })
+
+export const addItem_action = (id, state) => {
+       return ({
+        type: "spending_reducer/ADD_ITEM",
+        payload: {
+                id: id,
+                ...state,
+        }
+})}
--- src/redux/spending/spending_selectors.js
+++ src/redux/spending/spending_selectors.js
@@ -1,12 +1,26 @@
 import {createSelector} from "reselect"
-import {calculatMortgageBalance} from "services/general/mortgage_functions"
 
 
 const spending_reducer = state => state.spending_reducer
 
 //Spending SELECTORS
 
-export const mortgage_selector = createSelector(
-  [spending_reducer],
-  (spending_reducer) => Object.values(spending_reducer.asset).filter(d => d.subCategory === "mortgage")
+export const housing_selector = createSelector(
+    [spending_reducer],
+    (spending_reducer) => Object.values(spending_reducer.fixed).filter(d => d.subCategory === "housingcosts")                                                                   //creates a an array of each of the income subCategory names, which is used in the stacked Income chart
+)
+
+export const transportation_selector = createSelector(
+    [spending_reducer],
+    (spending_reducer) => Object.values(spending_reducer.fixed).filter(d => d.subCategory === "transportationcosts")                                                                   //creates a an array of each of the income subCategory names, which is used in the stacked Income chart
+)
+
+export const lifestyle_selector = createSelector(
+    [spending_reducer],
+    (spending_reducer) => Object.values(spending_reducer.asset).filter(d => d.subCategory === "lifestylecosts")                                                                   //creates a an array of each of the income subCategory names, which is used in the stacked Income chart
+)
+
+export const largeEvents_selector = createSelector(
+    [spending_reducer],
+    (spending_reducer) => Object.values(spending_reducer.asset).filter(d => d.subCategory === "largeEventsCosts")                                                                   //creates a an array of each of the income subCategory names, which is used in the stacked Income chart
 )
