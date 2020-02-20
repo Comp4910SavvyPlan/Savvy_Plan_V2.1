d01ad512aa9f02e54322ca01c4718094dabd5b43
Caleb <calebbelcourt@hotmail.com>
2020-02-20 15:28:29 -0800

    EditForm
    Added EditForm and implemented it into ControlPanel. Assumed that the spending_reducer and spending_data was complete so won't work until that is implemented

--- src/pages/spending/components/ControlPanel.js
+++ src/pages/spending/components/ControlPanel.js
@@ -1,15 +1,56 @@
 import React from "react"
 import styled from "styled-components"
+import {removeItem_action} from "redux/spending/spending_actions"
+import EditForm from "pages/netWorth/components/EditForm"
 //import ItemDisplayBox from "pages/spending/components/ItemDisplayBox"
+import {spending_data} from "pages/spending/data/spending_data"
 
 
-const ControlPanel = () => {
+const ControlPanel = ({setCount, display, spending_reducer}) => {
+
+      const [itemId, setItemId] = useState()                                                                      //If the user wants to change something this sets the id of the item they want to change
+
+      const [addFormSubCategory, setAddFormSubCategory] = useState()                                              //If wants to add something this sets the category of the item theyd like to add
+      const addFormDetails = spendingWizard_data.find(d => d.subCategory === addFormSubCategory)                  //Provides the add form with the details to render
+
+      const category = display                                                                                    //Display is either fixed or variable and is used to show either of those
+      const subCategory = itemId ? spending_reducer[category][itemId].subCategory : "housingCosts"                  //if we have an id we get the subCategory from the reducer, otherwise we set it to housingCosts
+
+
     return (
         <Wrapper>
             <Sections>
-              <Section>
-                control panel
-              </Section>
+            {
+                    itemId ?
+                    <EditForm
+                        itemId={itemId}                                                                        //Clicking add takes the id of the item being added and sets it in the local state
+                        category={category}
+                        subCategory={subCategory}
+                        setItemId={setItemId}
+                    />
+                    :
+                    addFormSubCategory ?
+                    //Add Form Here
+                    :
+                    <>
+                    {
+                        spending_data.map(d => (                                                       //if neither add or edit forms are clicked then it renders out the item display
+                            d.category === display ?
+                            <Section  key={d.subCategory}>
+                            <ItemDisplayBox                                                                  //Displays all the fixed or variables they have added
+                                category={d.category}
+                                item={d}
+                                subCategory={d.subCategory}
+                                setCount={setCount}
+                                setItemId={setItemId}
+                                setAddFormSubCategory={setAddFormSubCategory}
+                                />
+                            </Section>
+                            : null
+                        ))
+                    }
+                </>
+                 }
             </Sections>
         </Wrapper>
     )
@@ -33,8 +74,4 @@ const Sections = styled.div`
     padding: 1rem;
 
     justify-content: center;
-`
-const Section = styled.div`
-    width: 30%;
-    margin: 1rem;
 `
---
+++ src/pages/spending/components/EditForm.js
@@ -0,0 +1,154 @@
+import React from "react"
+import styled from "styled-components"
+import {connect} from "react-redux"
+import FormInput  from "UI/forms/Input"
+import DateInput from "UI/forms/DateInput"
+import RangeBar  from "UI/rangeBar/RangeBar"
+import MiniRangeBar  from "UI/miniRangeBar/MiniRangeBar"
+import ButtonLight from "UI/buttons/ButtonLight"
+import {setSpendingValue_action, changeLabel_action} from "redux/spending/spending_actions"
+import _ from "lodash"
+
+
+const EditForm = ({category, subCategory, setItemId, itemId, spending_reducer, setSpendingValue_action, changeLabel_action }) => {
+
+    const item = spending_reducer[category][itemId]                                             //uses the item id provided to go into the reducer and gather all the users details
+
+    const setValue = (logValue, rangeBarValue, rangeBarProps) => {                              //sets the value in the reducer
+        setSpendingValue_action(logValue, rangeBarValue, category, rangeBarProps, itemId)
+    }
+
+    const changeLabel = (e) => {                                                                //changes the label in the reducer
+        changeLabel_action(e, item, itemId)
+    }
+
+    return (
+        <>
+        <Wrapper>
+            <Header subCategory={subCategory}>
+            <h2>{_.startCase(subCategory)}</h2>
+            </Header>
+        <Container subCategory={subCategory}>
+
+                <Left>                                                                            {/*This div is empty and is used to push out the other divs to the same locations as the add form */}
+                </Left>
+
+                <Center>                                                                          {/*Very Similar to the add form */}
+                    <FormInput
+                        label="spending name"
+                        value={item.label}
+                        type={"text"}
+                        handleChange={changeLabel}
+                    />
+
+                    {
+                      //If the subcategory is one of these
+                        subCategory === "housingCosts" || subCategory === "transportationCosts" || subCategory === "lifestyleCosts" || subCategory === "largeEventsCosts" ?
+
+                        <RangeBar
+                            setValue=50
+                        /> : null
+                    }
+
+                <RangeBar
+                        setValue=60
+                    />
+
+                </Center>
+
+                <Right>
+                    <MiniRangeBarWrapper>
+                        {
+                          //if category is variable
+                            category === "variable" ?
+                            <MiniRangeBar
+                            setValue=10
+                        />
+                        :
+
+                        //if category is fixed
+                        subCategory === "housingCosts"  ?
+                            <MiniRangeBar
+                            setValue=20
+                         />
+                         :
+                         subCategory === "transportationCosts"  ?
+                            <MiniRangeBar
+                            setValue=30
+                      />
+
+
+                       : null
+                        }
+                    </MiniRangeBarWrapper>
+                    <ButtonWrapper>
+                            <ButtonLight
+                                text={"back"}
+                                onClick={() => setItemId(false)}
+                            />
+                    </ButtonWrapper>
+                </Right>
+            </Container>
+
+        </Wrapper>
+        </>
+
+    )
+
+}
+
+const mapStateToProps = (state) => ({
+    spending_reducer: state.spending_reducer
+})
+
+export default connect(mapStateToProps, {setSpendingValue_action, changeLabel_action})(EditForm )
+
+
+//-----------------------------------------------STYLES-----------------------------------------------//
+
+
+const Wrapper = styled.div`
+    width: 100rem;
+    border-radius: 5px;
+    overflow: hidden;
+    border: ${props => props.theme.border.primary};
+`
+const Header = styled.div`
+    width: 100%;
+    background: ${props => props.subCategory === "housingCosts" ? props.theme.color.blue :
+                  props => props.subCategory === "transportationCosts" ? props.theme.color.steelBlue :
+                  props => props.subCategory === "lifestyleCosts" ? props.theme.color.green :
+                  props => props.subCategory === "largeEventsCosts" ? props.theme.color.salmon :
+    null};
+    height: 4rem;
+    color: ${props => props.theme.color.ice};
+
+`
+const Left = styled.div`
+    width: 30rem;
+    height: 100%;
+    padding: 2rem;
+`
+const ButtonWrapper = styled.div`
+    position: absolute;
+    bottom: 2rem;
+    right: 2rem;
+`
+const MiniRangeBarWrapper = styled.div`
+    position: absolute;
+    right: 3rem;
+    top: 1rem;
+`
+const Right = styled.div`
+    width: 30rem;
+    padding: 2rem;
+`
+const Center = styled.div`
+    width: 45rem;
+    padding: 2rem;
+`
+const Container = styled.div`
+height: ${props => props.subCategory === "securedDebt" ? "33rem" : "30rem"};                                                    //mortgage requires more height because there are more inputs
+    display: flex;
+    position: relative;
+`
