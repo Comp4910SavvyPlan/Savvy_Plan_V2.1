d1f150b7b6ed0a834fd6af35ef9826db8f5fa1c1
Taki <takip84@gmail.com>
2020-02-20 18:16:36 -0800

    Caleb and Shohei 2

--- src/pages/spending/Spending.js
+++ src/pages/spending/Spending.js
@@ -1,41 +1,127 @@
-import React, {useState} from "react"
-import styled from "styled-components"
-import Header from "pages/spending/components/Header"
-import ControlPanel from "pages/spending/components/ControlPanel"
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import Header from "pages/spending/components/Header";
+import ControlPanel from "pages/spending/components/ControlPanel";
+import { connect } from "react-redux";
+import ButtonLight from "UI/buttons/ButtonLight";
+import { spendingWizard_data } from "pages/spending/data/spending_data";
+import WelcomePage from "pages/spending/components/WelcomePage";
+import Main from "pages/spending/components/Main";
 
 const Spending = () => {
+  const [count, setCount] = useState(0);
 
-    const [monthly, toggleMonthly] = useState(true)
+  const renderAddForm = data => {
+    return data.map(d =>
+      d.count === count ? (
+        <AddPage key={d.subCategory}>
+          <DisplayWrapper>
+            {" "}
+            {}
+            <ItemDisplayBox
+              category={d.category}
+              subCategory={d.subCategory}
+              setItemId={() => null}
+            />
+          </DisplayWrapper>
+          <AddFormWrapper>
+            {" "}
+            {}
+            <AddForm
+              category={d.category}
+              subCategory={d.subCategory}
+              currentValueLabel={d.currentValueLabel}
+              ageLabel={d.ageLabel}
+              expenseTypeArray={d.expenseTypeArray}
+              durationTypeArray={d.durationTypeArray}
+              setAddFormSubCategory={() => null}
+            />{" "}
+            {}
+          </AddFormWrapper>
+        </AddPage>
+      ) : null
+    );
+  };
 
-    return (
-        <Page>
-            <Header monthly={monthly} toggleMonthly={toggleMonthly}/>
-            <ChartWrapper>
-            </ChartWrapper>
-            <ControlPanel/>
+  return (
+    <Page>
+      {count === 0 ? (
+        <WelcomePage />
+      ) : count < 6 ? (
+        <>
+          {renderAddForm(spendingWizard_data)} {}
+        </>
+      ) : null}
+      <Buttons>
+        {" "}
+        {/* Fixed plan buttons enabling the toggling back and forth*/}
+        <ButtonLight
+          backward
+          onClick={() => setCount(count > 0 ? count - 1 : 0)}
+        />
+        <ButtonLight forward onClick={() => setCount(count + 1)} />
+      </Buttons>
+    </Page>
+  );
+};
 
+const mapStateToProps = state => ({});
 
-        </Page>
-    )
-}
-
-export default Spending
-
+export default connect(mapStateToProps, {})(Spending);
 
 //-----------------------------------------------STYLES-----------------------------------------------//
 
 const Page = styled.div`
-    ${props => props.theme.pageBaseStyles}
-    grid-template-rows: minmax(12rem, 14rem)  minmax(24rem, 28rem);
-    grid-template-areas: 
-    "a a a a "
-    "b b b b "
-    "c c c c "
-`
+  ${props => props.theme.pageBaseStyles}
+  grid-template-rows: minmax(7rem, 10rem) minmax(21rem, 22rem) minmax(28rem, 40rem);
+  width: 100%;
+  position: relative;
+  grid-template-areas:
+    "a a a a a a a a a a a a"
+    "b b b b b b b b b b b b"
+    "c c c c c c c c c c c c";
+`;
+const AddPage = styled(Page)``;
 
-const ChartWrapper = styled.div`
-    grid-area: b;
+const Charts = styled.div`
+  grid-area: b;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+`;
+const SunBurstChartPlaceHolder = styled.div`
+  width: 30%;
+  height: 90%;
+`;
+const ProjectionChartPlaceHolder = styled.div`
+  width: 70%;
+  height: 100%;
+`;
 
+const Buttons = styled.div`
+  position: absolute;
+  width: 141rem;
+  top: 30rem;
+  left: -10rem;
+  z-index: 100;
+  display: flex;
+  justify-content: space-between;
+`;
 
-`
+const DisplayWrapper = styled.div`
+  grid-area: a;
+  width: 80%;
+  margin-left: 10%;
+  height: 20rem;
+`;
+
+const AddFormWrapper = styled.div`
+  grid-area: c;
+  width: 80%;
+  margin-left: 10%;
+  border-radius: 5px;
+  overflow: hidden;
+  height: 35rem;
+`;
--- src/pages/spending/components/ControlPanel.js
+++ src/pages/spending/components/ControlPanel.js
@@ -1,12 +1,58 @@
 import React from "react"
 import styled from "styled-components"
+import {removeItem_action} from "redux/spending/spending_actions"
+import EditForm from "pages/netWorth/components/EditForm"
+//import ItemDisplayBox from "pages/spending/components/ItemDisplayBox"
+import {spending_data} from "pages/spending/data/spending_data"
 
 
-const ControlPanel = () => {
+const ControlPanel = ({setCount, display, spending_reducer}) => {
+
+      const [itemId, setItemId] = useState()                                                                      //If the user wants to change something this sets the id of the item they want to change
+
+      const [addFormSubCategory, setAddFormSubCategory] = useState()                                              //If wants to add something this sets the category of the item theyd like to add
+      const addFormDetails = spendingWizard_data.find(d => d.subCategory === addFormSubCategory)                  //Provides the add form with the details to render
+
+      const category = display                                                                                    //Display is either fixed or variable and is used to show either of those
+      const subCategory = itemId ? spending_reducer[category][itemId].subCategory : "housingCosts"                  //if we have an id we get the subCategory from the reducer, otherwise we set it to housingCosts
+
+
     return (
-        <Container>
-            control Panel
-        </Container>
+        <Wrapper>
+            <Sections>
+            {
+                    itemId ?
+                    <EditForm
+                        itemId={itemId}                                                                        //Clicking add takes the id of the item being added and sets it in the local state
+                        category={category}
+                        subCategory={subCategory}
+                        setItemId={setItemId}
+                    />
+                    :
+                    addFormSubCategory ?
+                    //Add Form Here
+                    :
+                    <>
+                    {
+                        spending_data.map(d => (                                                       //if neither add or edit forms are clicked then it renders out the item display
+                            d.category === display ?
+                            <Section  key={d.subCategory}>
+                            <ItemDisplayBox                                                                  //Displays all the fixed or variables they have added
+                                category={d.category}
+                                item={d}
+                                subCategory={d.subCategory}
+                                setCount={setCount}
+                                setItemId={setItemId}
+                                setAddFormSubCategory={setAddFormSubCategory}
+                                />
+                            </Section>
+                            : null
+                        ))
+                    }
+                </>
+                 }
+            </Sections>
+        </Wrapper>
     )
 }
 
@@ -15,6 +61,15 @@ export default ControlPanel
 
 //-----------------------------------------------style-----------------------------------------------//
 
-const Container = styled.div`
+const Wrapper = styled.div`
     grid-area: c;
+    width: 98%;
+    border-radius: 5px;
+    overflow: hidden;
+    margin-bottom: 1rem;
+`
+const Sections = styled.div`
+    display: flex;
+    padding: 1rem;
+    justify-content: center;
 `
