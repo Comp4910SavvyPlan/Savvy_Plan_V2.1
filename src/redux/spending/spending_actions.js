16c9393361bdd2f3cc5720a81dc9fbcf5f2f1c6b
Sho <shoyu99@hotmail.com>
2020-02-20 20:23:27 -0800

    Fixed Previous merge, back to original state

---
+++ src/redux/spending/spending_actions.js
@@ -0,0 +1,35 @@
+import _ from "lodash"
+
+export const setSpendingValue_action = (logValue, rangeBarValue, category, rangeBarProps, id) => {
+
+        return ({
+        type: "spending_reducer/SET_VALUE",
+        id: spendingId,
+        category: category,
+        financialValue: logValue,
+        rangeBarValue,
+        rangeBarProps
+})}
+
+export const changeLabel_action = (e, rangeBarProps, spendingId) => ({
+        type: "spending_reducer/CHANGE_LABEL",
+        id: spending,
+        label: e.target.value,
+        category: rangeBarProps.category,
+        rangeBarProps
+})
+
+export const removeSpending_action = (rangeBarProps) => ({
+        type: "spending_reducer/REMOVE_VALUE",
+        id: rangeBarProps.id,
+        category: rangeBarProps.category,
+})
+
+export const addItem_action = (id, state) => {
+       return ({
+        type: "spending_reducer/ADD_ITEM",
+        payload: {
+                id: id,
+                ...state,
+        }
+})}
